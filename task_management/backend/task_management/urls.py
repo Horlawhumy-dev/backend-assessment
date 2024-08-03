41badb2fbdef2e52bc71e57958e4ecba46b13cbc
Arafat Olayiwola <harof.dev@gmail.com>
2024-08-03 16:11:35 +0100

    add task app url

--- task_management/backend/task_management/urls.py
+++ task_management/backend/task_management/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls', namespace="users")),
-    # path('api/tasks/', include('tasks.urls', namespace="tasks"))
+    path('api/tasks/', include('tasks.urls', namespace="tasks"))
 ]
